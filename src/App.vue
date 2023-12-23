3330912b1d5268392ba47ac020f09c187a9b8db0
Jurate <dev.jurate@gmail.com>
2023-12-23 16:58:41 +0200

    Getting latitude and longitude of a client; setting global coordinates states with Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,30 @@
-<template></template>
+<template>
+  <main>
+    <div>lan: {{ getLan }} lon: {{ getLon }}</div>
+    <br />
+    <br />
+    <br />
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  computed: {
+    getLan() {
+      return this.$store.getters.getLatitude;
+    },
+
+    getLon() {
+      return this.$store.getters.getLongitude;
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch("getPreciseClientLocation");
+  },
+};
+</script>
