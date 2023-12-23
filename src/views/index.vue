a76d89f191723875ae67ec7fb99114b45b3cad69
Jurate <dev.jurate@gmail.com>
2023-12-23 20:23:50 +0200

    Included Card to index file

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,19 @@
 <template>
-  <Layout dark="true">
-    <div>lan: {{ getLan }} lon: {{ getLon }}</div>
+  <Layout dark>
+    <Card>
+      <div>lan: {{ getLan }} lon: {{ getLon }}</div>
+    </Card>
   </Layout>
 </template>
 
 <script>
 import Layout from "../layouts/base.vue";
+import Card from "../layouts/Card.vue";
 
 export default {
   components: {
     Layout,
+    Card,
   },
 
   computed: {
