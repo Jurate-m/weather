062fc8c15cb925c56c37d01a30706cb9b063888c
Jurate <dev.jurate@gmail.com>
2023-12-23 20:25:28 +0200

    Created Card component

---
+++ src/layouts/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import "../assets/scss/components/_card.scss";
+
+export default {
+  computed: {
+    classes() {
+      return {
+        card: true,
+      };
+    },
+  },
+};
+</script>
