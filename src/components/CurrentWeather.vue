785d45e944ad60f8faa17af64de2b20b2f5d78f5
Jurate <dev.jurate@gmail.com>
2023-12-27 12:55:52 +0200

    Components for current weather card

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,42 @@
+<template>
+  <Card>
+    <Row>
+      <Col class="col-6">
+        <Search></Search>
+      </Col>
+      <Col class="col-6">
+        <h5>Vilnius</h5>
+      </Col>
+    </Row>
+    <h3 style="text-align: center">Thursday, 11:47</h3>
+    <div style="margin-bottom: 4em">
+      <img style="display: block; margin: 0 auto" src="@/assets/icons/2.png" />
+    </div>
+    <h4 style="text-align: center">Sunny</h4>
+    <h3 style="text-align: center">25<button>°C</button><button>°F</button></h3>
+    <h5 style="text-align: center">Feels like +29</h5>
+  </Card>
+</template>
+
+<script>
+import Search from "./Search.vue";
+import Card from "@/layouts/Card.vue";
+import Row from "@/layouts/Row.vue";
+
+import Col from "@/layouts/Col.vue";
+
+export default {
+  props: [],
+
+  components: {
+    Card,
+    Search,
+    Row,
+    Col,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<!-- NTH -->
+
+<template>
+  <div :class="classes">
+    <div>
+      <form>
+        <input type="text" name="location-query" placeholder="Location" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../assets/scss/components/_search.scss";
+
+export default {
+  props: [],
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    classes() {
+      return {
+        "search-box": true,
+      };
+    },
+  },
+};
+</script>
