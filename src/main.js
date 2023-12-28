2a939cb88a6a5360b71d8eb794c5792a4cab5b75
Jurate <dev.jurate@gmail.com>
2023-12-28 22:37:29 +0200

    Created custom directive for handling clicking off element

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
-// import router from "./router.js";
-import store from "./store.js";
+import store from "./store/index.js";
 import App from "./App.vue";
+import ClickOutsideDirective from "./directives/ClickOutsideDirective.js";
 
 const app = createApp(App);
-// app.use(router);
+app.directive("click-out", ClickOutsideDirective);
 app.use(store);
 app.mount("#app");
