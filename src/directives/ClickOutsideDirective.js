ff141ad6b23a6677d20f23f17b24927db08cadab
Jurate <dev.jurate@gmail.com>
2024-01-03 22:50:29 +0200

    Removed console logs

--- src/directives/ClickOutsideDirective.js
+++ src/directives/ClickOutsideDirective.js
@@ -1,8 +1,5 @@
 export default {
   beforeMount: (el, binding) => {
-    console.log(el);
-    console.log(binding);
-
     el.eventHandler = function (e) {
       if (!(el === e.target || el.contains(e.target))) {
         binding.value(e);
