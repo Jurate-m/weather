d7d2fb341a682c856103b27a8de707712c6676d4
Jurate <dev.jurate@gmail.com>
2024-01-03 22:53:16 +0200

    Included weather module

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,13 @@ import { createStore } from "vuex";
 import rootActions from "./actions.js";
 import rootGetters from "./getters.js";
 import rootMutations from "./mutations.js";
+import weatherModule from "./modules/weather/index.js";
 
 const store = createStore({
+  modules: {
+    weather: weatherModule,
+  },
+
   state() {
     return {
       latitude: null,
