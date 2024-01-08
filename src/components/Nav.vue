674147b4e96b6bd9d48f06cd3aef4fbb5670c35e
Jurate <dev.jurate@gmail.com>
2024-01-08 16:59:14 +0200

    Nav component creation

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="nav">
+    <ul>
+      <li v-for="link in links" :key="link.id">
+        <router-link :to="link.url">{{ link.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import "../assets/scss/components/_nav.scss";
+
+export default {
+  data() {
+    return {
+      links: [
+        { id: 1, name: "Current", url: "/" },
+        { id: 2, name: "Hourly", url: "/hourly" },
+        { id: 3, name: "Daily", url: "/daily" },
+      ],
+    };
+  },
+};
+</script>
