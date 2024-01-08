7949da3441de33e23964eefa643e1993d6c4a749
Jurate <dev.jurate@gmail.com>
2024-01-08 16:55:36 +0200

    Router setup

--- src/router.js
+++ src/router.js
@@ -1,3 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import Home from "./views/Home.vue";
+import Hourly from "./views/Hourly.vue";
+import Daily from "./views/Daily.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/hourly", component: Hourly },
+    { path: "/daily", component: Daily },
+  ],
+});
+
 export default router;
