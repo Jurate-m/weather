9972954279fbfb1761686dbb46f96448489e8bd6
Jurate <dev.jurate@gmail.com>
2024-01-31 19:40:34 +0200

    Root action for retrieving client location

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,36 +1,68 @@
+import axios from "axios";
+import apiRequest from "./utils/apiUtils.js";
+
 export default {
-  setClientLocation({ commit, dispatch }) {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.watchPosition(
-        (position) => {
-          commit("setLatitude", position.coords.latitude);
-          commit("setLongitude", position.coords.longitude);
-          commit("enableLocation");
-        },
-        (error) => {
-          console.error("Error getting user location:", error);
-          alert(
-            "Enable location permission to get results based on more accurate coordinates"
-          );
-          dispatch("getClientLocation");
-        },
-        { enableHighAccuracy: true }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
+  async setUserLocation({ commit }) {
+    return new Promise((resolve, reject) => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.watchPosition(
+          (position) => {
+            commit("enableLocation");
+            commit("setLatitude", position.coords.latitude);
+            commit("setLongitude", position.coords.longitude);
+            resolve();
+          },
+          (error) => {
+            console.error(error);
+            reject(error);
+          },
+          { enableHighAccuracy: true }
+        );
+      }
+    });
   },
 
-  async getClientLocation({ commit }) {
-    await fetch("https://ipapi.co/json/")
+  getUserLocation({ commit }) {
+    return axios
+      .get("https://ipapi.co/json")
       .then((response) => {
-        return response.json();
+        commit("setLatitude", response.data.latitude);
+        commit("setLongitude", response.data.longitude);
       })
-      .then((data) => {
-        commit("setLatitude", data.latitude);
-        commit("setLongitude", data.longitude);
-        commit("setPlaceId", data.city);
-      })
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => console.error(error));
+  },
+
+  async getLocationId({ state, commit }) {
+    const lat = state.latitude || sessionStorage.getItem("Latitude");
+    const lon = state.longitude || sessionStorage.getItem("Longitude");
+    if (!lat || !lon) return;
+
+    await apiRequest(
+      "nearest_place",
+      { lat: lat, lon: lon },
+      commit,
+      "setLocationId"
+    );
+  },
+
+  async loadLocationId({ state, dispatch }) {
+    try {
+      await dispatch("setUserLocation");
+    } catch (error) {
+      try {
+        await dispatch("getUserLocation");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (!state.latitude || !state.longitude) return;
+
+    try {
+      await dispatch("getLocationId");
+      console.log("locationID");
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
