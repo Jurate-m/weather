88a0f10a38f8907ed1b29aae070398af5147e452
Jurate <dev.jurate@gmail.com>
2024-01-31 19:38:11 +0200

    Vuex store module responsibe for retrieving and managing current weather data

---
+++ src/store/modules/current.js
@@ -0,0 +1,37 @@
+import apiRequest from "../utils/apiUtils.js";
+
+export default {
+  namespaced: true,
+
+  state() {
+    return {
+      currentWeather: null,
+    };
+  },
+
+  mutations: {
+    assignCurrentWeather(state, payload) {
+      state.currentWeather = payload.data.current;
+      sessionStorage.setItem(
+        "CurrentWeather",
+        JSON.stringify(payload.data.current)
+      );
+    },
+  },
+
+  actions: {
+    async getCurrentWeather({ rootState, commit }) {
+      const place_id =
+        rootState.locationId || sessionStorage.getItem("LocationId");
+
+      if (!place_id) return;
+
+      await apiRequest(
+        "current",
+        { place_id: place_id },
+        commit,
+        "assignCurrentWeather"
+      );
+    },
+  },
+};
