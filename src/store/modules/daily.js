ec020d032ca004751739a2fb4210a5e58b1cfe04
Jurate <dev.jurate@gmail.com>
2024-01-31 19:38:53 +0200

    Vuex store module responsibe for retrieving and managing daily weather data

---
+++ src/store/modules/daily.js
@@ -0,0 +1,37 @@
+import apiRequest from "../utils/apiUtils.js";
+
+export default {
+  namespaced: true,
+
+  state() {
+    return {
+      dailyWeather: [],
+    };
+  },
+
+  mutations: {
+    assignDailyWeather(state, payload) {
+      state.dailyWeather = payload.data.daily.data;
+      sessionStorage.setItem(
+        "DailyWeather",
+        JSON.stringify(payload.data.daily.data)
+      );
+    },
+  },
+
+  actions: {
+    async getDailyWeather({ commit, rootState }) {
+      const place_id =
+        rootState.locationId || sessionStorage.getItem("LocationId");
+
+      if (!place_id) return;
+
+      await apiRequest(
+        "daily",
+        { place_id: place_id },
+        commit,
+        "assignDailyWeather"
+      );
+    },
+  },
+};
