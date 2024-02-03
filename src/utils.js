358f31aa35db85071ebc6d1a51a390f5f6ed907a
Jurate <dev.jurate@gmail.com>
2024-02-03 22:49:04 +0200

    Utility functions for string modification and date formating

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+export function modifyString(string) {
+  string = string.replace(/[^a-zA-Z ]/g, " ");
+  let modifiedString = null;
+  if (string.split(" ").length > 1) {
+    modifiedString = string
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  } else {
+    modifiedString = string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  return modifiedString;
+}
+
+export function formatDate(date, options) {
+  const formatOptions = new Intl.DateTimeFormat("en-GB", options);
+  return formatOptions.format(date);
+}
