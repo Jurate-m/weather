164e94c63ac5ccf1ead53536674cf03a6162ca1f
Jurate <dev.jurate@gmail.com>
2024-02-03 22:52:47 +0200

    Listing component restruction

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,42 +1,18 @@
 <template>
-  <div :class="classes" v-if="data">
-    <h3>
-      <slot name="header"> Test </slot>
-    </h3>
+  <div :class="classes">
+    <h2>
+      <slot name="header"></slot>
+    </h2>
     <ul>
-      <div>
-        <ListingSingle
-          v-for="item in data"
-          :data="item"
-          v-if="data"
-        ></ListingSingle>
-        <br />
-        <br />
-        <br />
-      </div>
+      <slot></slot>
     </ul>
   </div>
 </template>
 
 <script>
-import ListingSingle from "./ListingSingle.vue";
-
 export default {
-  components: { ListingSingle },
-  props: ["data"],
-
   data() {
-    return {
-      weekdays: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ],
-    };
+    return {};
   },
 
   methods: {
