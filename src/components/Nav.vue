f06e92ba6d4333c1a51cb66661c8ae194f870846
Jurate <dev.jurate@gmail.com>
2024-02-19 20:03:09 +0200

    Navigation: added burger menu; set scroll class; dynamic links rendering using router

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,48 @@
 <template>
-  <nav class="nav">
-    <ul>
-      <li v-for="link in links" :key="link.id">
-        <router-link :to="link.url">{{ link.name }}</router-link>
-      </li>
-    </ul>
+  <nav :class="parentClass" v-click-out="hide">
+    <button class="nav-burger" @click="toggleDisplay" type="button">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+    <Transition>
+      <ul class="nav-list" v-show="this.active">
+        <li v-for="route in routes" :key="route.name">
+          <router-link :to="route.path">{{ route.name }}</router-link>
+        </li>
+      </ul>
+    </Transition>
   </nav>
 </template>
 
 <script>
-import "../assets/scss/components/_nav.scss";
+import "@/assets/scss/components/_nav.scss";
 
 export default {
   data() {
     return {
-      links: [
-        { id: 1, name: "Current", url: "/" },
-        { id: 2, name: "Hourly", url: "/hourly" },
-        { id: 3, name: "Daily", url: "/daily" },
-      ],
+      routes: this.$router.options.routes,
+      active: false,
     };
+  },
+
+  computed: {
+    parentClass() {
+      return {
+        nav: true,
+        active: this.active,
+      };
+    },
+  },
+
+  methods: {
+    toggleDisplay() {
+      this.active = !this.active;
+    },
+
+    hide() {
+      this.active = false;
+    },
   },
 };
 </script>
