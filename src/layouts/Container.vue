20653c094587cd32a3c040a44c3957ce6aeab2b8
Jurate <dev.jurate@gmail.com>
2024-02-19 20:11:29 +0200

    Layout changed to Container - new component with styles

---
+++ src/layouts/Container.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import "../assets/scss/main.scss";
+import "../assets/scss/layouts/_container.scss";
+
+export default {};
+</script>
