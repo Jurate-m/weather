b1e65e750273bf3c2f06116b55eea47c6cd3282d
Jurate <dev.jurate@gmail.com>
2024-03-09 19:12:12 +0200

    Added Netlify CLI and Netflify function to handle requests

---
+++ netlify/functions/weather.mjs
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const handler = async (event, context) => {
+  console.log({ event }, { context });
+
+  const parameters = event.queryStringParameters;
+  const endpoint = parameters.endpoint;
+  delete parameters.endpoint;
+
+  const options = {
+    method: "GET",
+    url: `https://ai-weather-by-meteosource.p.rapidapi.com/${endpoint}`,
+    params: parameters,
+    headers: {
+      "X-RapidAPI-Key": process.env.VITE_RAPIDAPI_KEY,
+      "X-RapidAPI-Host": process.env.VITE_RAPIDAPI_HOST,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.toString(),
+    };
+  }
+};
