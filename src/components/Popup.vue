77263f80f8a0c0bbfbe991dc8e49da73d0215ef5
Jurate <dev.jurate@gmail.com>
2024-03-16 23:32:34 +0200

    Popup component

---
+++ src/components/Popup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="popup">
+    <div class="popup__card">
+      <p class="mb-20">{{ message }}</p>
+      <button @click="popupAction">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/assets/scss/components/_popup.scss";
+
+export default {
+  emits: ["popupAction"],
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    popupAction() {
+      this.$emit("popupAction");
+    },
+  },
+};
+</script>
