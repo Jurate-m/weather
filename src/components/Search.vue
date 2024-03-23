84cfc8d55194985721bb3cf4318f3631161f5b64
Jurate <dev.jurate@gmail.com>
2024-03-23 22:50:35 +0200

    Search component: input validation

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,11 @@
 <template>
-  <div :class="classes" v-click-out="hide">
-    <div class="search__trigger" @click="show()">
+  <div class="search" :class="activeClass" @click="show()" v-click-out="clear">
+    <div class="search__trigger">
       <form @submit.prevent="sendData()">
         <div class="search__inner">
-          <!-- Maybe add @click.prevent to prevent from submitting??? -->
           <button
-            :type="active ? 'submit' : 'button'"
-            :aria-label="active ? 'Open Search' : 'Submit'"
+            :type="validSubmit ? 'submit' : 'button'"
+            :aria-label="validSubmit ? 'Search' : 'Open Search'"
           >
             <img src="/assets/icons/search.svg" alt="search icon" />
           </button>
@@ -17,138 +16,260 @@
             v-model="locationInput"
             aria-errormessage="err"
           />
+          <button
+            class="search__clear"
+            type="button"
+            @click.prevent="clearInput()"
+            aria-label="Clear input"
+            v-show="trimmedInput && active"
+          ></button>
         </div>
-        <!-- <p v-if="error" id="err" class="search__error">
+        <p v-if="inputError" id="err" class="search__error">
           Please use only alphabetical or numerical values in Location search.
-        </p> -->
-        <!-- <div
+        </p>
+        <div
           v-else
           class="search__dropdown"
-          :class="dropdownClass"
-          v-show="active"
+          v-show="active && (locationArr.length || !currentLocationPermission)"
         >
-          <div class="searh__dropdown-location">
-            <button
-              v-if="!locationEnabled"
-              @click.prevent="getCurrentLocation"
-              type="button"
-            >
-              Use Current Location
+          <div
+            v-if="!currentLocationPermission"
+            class="search__current-location"
+          >
+            <button @click.prevent="useCurrentLocation()">
+              Use current location
             </button>
           </div>
-          <fieldset v-show="locationArr">
-            <legend>Locations</legend>
-            <div v-for="item in locationArr" class="search__dropdown-item">
-              <input
-                type="radio"
-                :id="item.place_id"
-                :value="item"
-                v-model="selectedLocation"
-                @change.prevent="sendData()"
-                :aria-label="item.name"
-              />
-
-              <label :for="item.place_id">{{ item.name }}</label>
-            </div>
-          </fieldset>
-        </div> -->
+          <ul v-if="locationArr.length">
+            <li
+              v-for="item in locationArr"
+              class="search__dropdown-item"
+              :key="item.place_id"
+            >
+              <button type="button" @click.prevent="selectLocation(item)">
+                {{ item.name }}
+              </button>
+            </li>
+          </ul>
+        </div>
       </form>
     </div>
+    <Teleport to="body">
+      <Popup
+        v-if="activePopup"
+        :message="currentLocationError"
+        @popupAction="closePopup()"
+      ></Popup>
+    </Teleport>
+    <div></div>
   </div>
-  <!-- <Teleport to="body"
-    ><Popup v-if="errorPopup" :message="errorPopup" @popupAction="closePopup"></Popup
-  ></Teleport> -->
 </template>
 
 <script setup>
-import "@/assets/scss/components/_search.scss";
-
-import { ref, computed, reactive, watch } from "vue";
+import { ref, computed, watch } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
-
-const regex = /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/;
-
-const active = ref(false);
-const locationInput = ref(null);
-const locationArr = reactive([]);
 const selectedLocation = ref(null);
-const timer = ref(null);
-const place_id = ref(null);
-const place_name = ref(null);
-const error = ref(false);
-const errorPopup = ref(null);
-const locationEnabled = ref(false);
-const inputError = ref(false);
 
-// CONVERT TO STRING JUST IN CASE
+// Generating Locations List from user input START----------------------------
+const locationInput = ref(null);
+const locationArr = ref([]);
 
 // remove whitespace around locationInput
-const trimmedInputValue = computed(() => {
+const trimmedInput = computed(() => {
   if (locationInput.value) return locationInput.value.trim();
 });
 
-// Check and return a boolena if trimmed input value contains provided regex
-const invalidInput = computed(() => {
-  return regex.test(trimmedInputValue.value);
+// computed property for changing form button properties
+const validSubmit = computed(() => {
+  return active.value && trimmedInput.value && !inputError.value;
 });
 
-// maybe write a simple function and call it with an arg
+async function getLocationList() {
+  if (!trimmedInput.value) return;
+
+  const params = new URLSearchParams({
+    endpoint: "find_places_prefix",
+    text: locationInput.value.toString(),
+    language: "en",
+  });
+
+  return await fetch(`/.netlify/functions/weather?${params.toString()}`)
+    .then((response) => response.json())
+    .then((response) => {
+      response.forEach((item) => {
+        locationArr.value.push({
+          name: item.name,
+          admin_area: item.adm_area1,
+          place_id: item.place_id,
+        });
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+const timer = ref(null);
+
+function renderLocationList() {
+  if (timer.value) {
+    clearTimeout(timer.value);
+    timer.value = null;
+  }
+  timer.value = setTimeout(getLocationList, 1000);
+}
+
+// Generating Locations List from user input END----------------------------
+
+// Location Input validation START----------------------------
+const regex = /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/;
+const inputError = ref(false);
+
 function testingRegex(arg) {
   return regex.test(arg);
 }
 
+// Check and return a boolena if trimmed input value contains provided regex
+const invalidInput = computed(() => {
+  return testingRegex(trimmedInput.value);
+});
+
 // watch if invalidInput computed property changes and assign returned value to input error state
 watch(invalidInput, (newValue) => {
-  // this feels hacky
-  // if there is a new Value (which would be TRUE) and that value is TRUE
-  if (newValue && newValue === true) inputError.value = newValue;
-  // change back inputError to false AFTER something
+  inputError.value = newValue;
 });
 
-watch(trimmedInputValue, (newValue, oldValue) => {
-  if (trimmedInputValue.value) {
-    console.log(inputError.value);
-    // //jesus christ what a comparison
-    // if (
-    //   // check if there is no oldValue OR
-    //   !oldValue ||
-    //   // check if there is an oldValue AND
-    //   (oldValue &&
-    //     !testingRegex(newValue) &&
-    //     testingRegex(oldValue) &&
-    //     // if it contains new value, AND
-    //     !oldValue.startsWith(newValue) &&
-    //     // if the new Value is shorter than the old string
-    //     newValue.length > oldValue.length)
-    // ) {
-    //   renderLocationList();
-    //   // !!!!! I need to include error check (inputError.value) if the
-    //   // it should fire on every event, right?
-    //   // So how do I exclude startsWith and lenght check if on that particular change there was an error and now it is gone...?
-    // }
-    if (oldValue) {
-      console.log();
+watch(trimmedInput, (newValue, oldValue) => {
+  if (trimmedInput.value) {
+    // variable for checking if oldValue exists AND if it starts with a newValue
+    let oldValueStartsWithNewValue = oldValue && oldValue.startsWith(newValue);
+
+    // variable for checking if oldValue does have spaecial characters
+    let oldValueContainsRegex = testingRegex(oldValue);
+
+    // comparison variable for:
+    let passCondition_1 =
+      // checking IF oldValue exists AND
+      oldValue &&
+      // oldValue starts with newValue AND
+      oldValueStartsWithNewValue &&
+      // olValue has special characters AND
+      oldValueContainsRegex &&
+      // if there is no special charakters in current input value
+      !inputError.value;
+
+    let passCondition_2 =
+      oldValue && !oldValueStartsWithNewValue && !inputError.value;
+
+    let passCondition_3 =
+      oldValue &&
+      // oldValue starts with newValue AND
+      oldValueStartsWithNewValue &&
+      // there is no input error AND
+      !inputError.value &&
+      // there are no available locations
+      !locationArr.value.length;
+
+    if (
+      // there is no oldValue and input value doesn't contain error OR
+      (!oldValue && !inputError.value) ||
+      // there is no oldValue and input value doesn't contain error OR
+      passCondition_1 ||
+      passCondition_2 ||
+      passCondition_3
+    ) {
+      locationArr.value = [];
+      renderLocationList();
+    }
+  } else {
+    locationArr.value = [];
+  }
+});
+
+// Location Input validation END----------------------------
+
+// Current location START ----------------------------------
+const currentLocationPermission = ref(false);
+const activePopup = ref(false);
+
+function geolocationStateHandler(state) {
+  currentLocationPermission.value = state === "granted";
+}
+
+// Event listener for permissions change
+navigator.permissions
+  .query({ name: "geolocation" })
+  .then((permissionStatus) => {
+    geolocationStateHandler(permissionStatus.state);
+
+    permissionStatus.onchange = () => {
+      geolocationStateHandler(permissionStatus.state);
+    };
+  });
+
+const currentLocationError = computed(() => {
+  return store.state.location.error;
+});
+
+function useCurrentLocation() {
+  store.dispatch("location/getCurrentUserLocation");
+
+  activePopup.value = !!currentLocationError.value;
+}
+
+function closePopup() {
+  activePopup.value = false;
+}
+
+// Current location END ----------------------------------
+
+function clear() {
+  selectedLocation.value = {};
+  locationArr.value = [];
+  locationInput.value = null;
+  active.value = false;
+}
+
+function selectLocation(data) {
+  selectedLocation.value = data;
+  sendData();
+}
+
+function sendData() {
+  if (
+    // there is a value in the input AND
+    trimmedInput.value &&
+    // there are no errors AND
+    !inputError.value &&
+    // there are generated locations
+    locationArr.value.length
+  ) {
+    // define location ID
+    const place_id = selectedLocation.value
+      ? selectedLocation.value.place_id
+      : locationArr[0].value.place_id;
+
+    // define location name
+    const place_name = selectedLocation.value
+      ? selectedLocation.value.name
+      : locationArr[0].value.name;
+
+    if (place_id && place_name) {
+      store.dispatch("location/assignLocationId", place_id);
+      store.dispatch("location/assignLocationName", place_name);
+
+      clear();
     }
   }
+}
 
-  // call renderLocations array only if value has changed and
-  // if doesn't start the same as old value and length is bigger?
-  // (startsWith js function)
-  // console.log(!!newVal); to return a bool
-});
+const active = ref(false);
 
-const classes = computed(() => {
+const activeClass = computed(() => {
   return {
-    search: true,
-    "search--active": active.value,
-  };
-});
-
-const dropdownClass = computed(() => {
-  return {
-    active: active.value && locationArr.value,
+    active: active.value,
   };
 });
 
@@ -156,97 +277,17 @@ function show() {
   active.value = true;
 }
 
-function hide() {
-  active.value = false;
-}
-
-// function closePopup() {
-//   errorPopup.value = null;
-// }
-
-function getCurrentLocation() {
-  store.dispatch("location/getCurrentUserLocation").catch((error) => {
-    switch (error.code) {
-      case 1:
-        errorPopup.value =
-          "Please enable location permission to retrieve data for current location.";
-        break;
-      case 2:
-        errorPopup.value = "Location information is unavailable.";
-        break;
-      case 3:
-        errorPopup.value = "The request to get user location timed out.";
-        break;
-      default:
-        errorPopup.value = "Unknown error occured.";
-    }
-  });
-}
-
-async function getLocationList() {
-  if (locationInput.value) {
-    locationArr.value = [];
-
-    error.value = false;
-
-    try {
-      const params = new URLSearchParams({
-        endpoint: "find_places_prefix",
-        text: locationInput.value.toString(),
-        language: "en",
-      });
-
-      const resp = await fetch(
-        `/.netlify/functions/weather?${params.toString()}`
-      );
-
-      const data = await resp.json();
-
-      data.forEach((item) => {
-        locationArr.value.push({
-          name: item.name,
-          admin_area: item.adm_area1,
-          place_id: item.place_id,
-        });
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    locationArr.value = [];
-    error.value = false;
-  }
-}
-
-function testTimerFunciton() {
-  console.log("testing timer");
-}
-
-function renderLocationList() {
-  if (timer.value) {
-    clearTimeout(timer.value);
-    timer.value = null;
-  }
-  timer.value = setTimeout(testTimerFunciton, 1000);
-}
-
-function sendData() {
-  if (locationInput.value && locationArr.value && !error.value) {
-    place_id.value = selectedLocation.value
-      ? selectedLocation.value.place_id
-      : locationArr[0].value.place_id;
-    place_name.value = selectedLocation.value
-      ? selectedLocation.value.name
-      : locationArr[0].value.name;
-
-    store.dispatch("location/setUserLocationId", place_id.value);
-    store.dispatch("location/setUserLocationName", place_name.value);
-
-    hide();
-  }
+function clearInput() {
+  locationInput.value = null;
 }
 </script>
 
 <script>
-export default {};
+import "@/assets/scss/components/_search.scss";
+
+import Popup from "@/components/Popup.vue";
+
+export default {
+  components: { Popup },
+};
 </script>
