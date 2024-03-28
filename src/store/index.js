1552ec83c5c167d6c279c108f7faafe672477670
Jurate <dev.jurate@gmail.com>
2024-03-28 17:33:09 +0200

    Empty astro vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 
 import locationModule from "./modules/location/index.js";
 import weathreModule from "./modules/weather/index.js";
+import astroModule from "./modules/astro/index.js";
 
 const store = createStore({
   modules: {
     location: locationModule,
     weather: weathreModule,
+    astro: astroModule,
   },
 });
 
---
+++ src/store/modules/astro/index.js
@@ -0,0 +1,10 @@
+import astroMutations from "./mutations.js";
+import astroActions from "./actions.js";
+
+export default {
+  namespaced: true,
+
+  mutations: astroMutations,
+
+  actions: astroActions,
+};
