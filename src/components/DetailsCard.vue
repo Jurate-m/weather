baa41e7004282cdf22bbeb54e32fa4366d5142c7
Jurate <dev.jurate@gmail.com>
2024-04-03 21:54:41 +0300

    DetailsCard component amends to ensure reusability

--- src/components/DetailsCard.vue
+++ src/components/DetailsCard.vue
@@ -1,29 +1,13 @@
 <template>
   <div class="details-card">
     <ul>
-      <li class="mb-30">
-        <h3 class="mb-10 c-secondary">Feels like:</h3>
-        <p>{{ Math.round(data.feels_like) }}°C</p>
-      </li>
-      <li class="mb-30">
-        <h3 class="mb-10 c-secondary">Precipitation</h3>
-        <p>{{ data.probability.precipitation }}%</p>
-      </li>
-      <li class="mb-30">
-        <h3 class="mb-10 c-secondary">Humidity:</h3>
-        <p>{{ data.humidity }}%</p>
-      </li>
-      <li class="mb-30">
-        <h3 class="mb-10 c-secondary">Wind:</h3>
-        <p>{{ data.wind.dir }} {{ data.wind.speed }}m/s</p>
-      </li>
-      <li class="mb-30">
-        <h3 class="mb-10 c-secondary">UV index:</h3>
-        <p>{{ data.uv_index }} of 11</p>
-      </li>
-      <li class="mb-30">
-        <h3 class="mb-10 c-secondary">Pressure:</h3>
-        <p>{{ data.pressure }}hPa</p>
+      <li
+        v-for="{ title, value } in details"
+        :class="{ separated: separator }"
+        :key="new Date()"
+      >
+        <h3 class="mb-10 c-secondary">{{ title }}:</h3>
+        <p>{{ value }}</p>
       </li>
     </ul>
   </div>
@@ -34,12 +18,15 @@ import "@/assets/scss/components/_detailsCard.scss";
 
 export default {
   props: {
-    data: {
+    details: {
       type: Object,
       required: true,
     },
-  },
 
-  computed() {},
+    separator: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
